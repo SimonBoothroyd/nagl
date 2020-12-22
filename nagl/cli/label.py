f2f3bdeda8f1279f88b7db1360fa56ea278b1b0f
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-12-22 15:01:54 +0000

    Cleanup the label CLI.

--- nagl/cli/label.py
+++ nagl/cli/label.py
@@ -1,13 +1,16 @@
+import math
 import pickle
 import sys
 from typing import List, Optional, Tuple
 
 import click
+from click_option_group import optgroup
 from dask import distributed
 from dask_jobqueue import LSFCluster
 from distributed import LocalCluster, as_completed
 from openeye import oechem
 from openforcefield.topology import Molecule
+from tqdm import tqdm
 
 from nagl.labels.am1 import compute_am1_charge_and_wbo
 from nagl.utilities.openeye import enumerate_tautomers, guess_stereochemistry
@@ -24,7 +27,9 @@ def label_molecule_batch(
         AM1 charges and WBO if no exceptions were raised (``None`` otherwise) and the
         error string if an exception was raised (``None`` otherwise).
     """
-    return [compute_am1_charge_and_wbo(oe_molecule) for oe_molecule in oe_molecules]
+    return [
+        compute_am1_charge_and_wbo(oe_molecule) for oe_molecule in tqdm(oe_molecules)
+    ]
 
 
 @click.command(
@@ -36,13 +41,21 @@ def label_molecule_batch(
     help="The path to the input molecules. This should either be an SDF or a Gzipped "
     "file.",
     type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=True,
 )
 @click.option(
     "--output",
     "output_path",
-    default="labelled-molecules.pkl",
     help="The path to save the labelled and pickled molecules to.",
     type=click.Path(exists=False, file_okay=True, dir_okay=False),
+    required=True,
+)
+@click.option(
+    "--n-workers",
+    help="The number of workers to distribute the labelling across. Use -1 to request "
+    "one worker per batch.",
+    type=int,
+    required=True,
 )
 @click.option(
     "--worker-type",
@@ -52,33 +65,36 @@ def label_molecule_batch(
     help="The type of worker to distribute the labelling across.",
 )
 @click.option(
-    "--n-workers",
-    help="The number of workers to distribute the labelling across.",
+    "--batch-size",
+    help="The number of molecules to processes at once on a particular worker.",
     type=int,
+    default=500,
+    show_default=True,
 )
-@click.option(
-    "--worker-memory",
+@optgroup.group("LSF configuration", help="Options configure LSF workers.")
+@optgroup.option(
+    "--lsf-memory",
     help="The amount of memory (GB) to request per LSF queue worker.",
+    type=int,
     default=3,
     show_default=True,
-    type=int,
 )
-@click.option(
-    "--worker-time",
+@optgroup.option(
+    "--lsf-walltime",
     help="The maximum wall-clock time to request per LSF queue worker.",
+    type=str,
     default="02:00",
     show_default=True,
-    type=str,
 )
-@click.option(
-    "--queue",
+@optgroup.option(
+    "--lsf-queue",
     help="The LSF queue to submit workers to.",
     type=str,
     default="default",
     show_default=True,
 )
-@click.option(
-    "--conda-env",
+@optgroup.option(
+    "--lsf-env",
     help="The conda environment that LSF workers should run using.",
     type=str,
 )
@@ -86,17 +102,18 @@ def label(
     input_path: str,
     output_path: str,
     worker_type: str,
-    worker_memory: int,
-    worker_time: str,
     n_workers: int,
-    queue: str,
-    conda_env: str,
+    batch_size: int,
+    lsf_memory: int,
+    lsf_walltime: str,
+    lsf_queue: str,
+    lsf_env: str,
 ):
 
     input_molecule_stream = oechem.oemolistream()
     input_molecule_stream.open(input_path)
 
-    print("Enumerating tautomers")
+    print(" - Enumerating tautomers")
 
     output_stream = oechem.oeosstream()
 
@@ -111,23 +128,36 @@ def label(
     input_molecule_stream.close()
     oechem.OEThrow.SetOutputStream(oechem.oeerr)
 
-    print("Stripping salts")
+    print(" - Stripping salts")
 
     for oe_molecule in enumerated_molecules:
         oechem.OEDeleteEverythingExceptTheFirstLargestComponent(oe_molecule)
 
-    print("Labeling molecules")
+    print(" - Labeling molecules")
+
+    n_batches = int(math.ceil(len(enumerated_molecules) / batch_size))
+
+    if n_workers < 0:
+        n_workers = n_batches
+
+    if n_workers > n_batches:
+        print(
+            f"\n    [WARNING] More workers were requested then there are batches to "
+            f"compute. Only {n_batches} workers will be requested."
+        )
+
+        n_workers = n_batches
 
     # Set-up dask to distribute the processing.
     if worker_type == "lsf":
         dask_cluster = LSFCluster(
-            queue=queue,
+            queue=lsf_queue,
             cores=1,
-            memory=f"{worker_memory * 1e9}B",
-            walltime=worker_time,
+            memory=f"{lsf_memory * 1e9}B",
+            walltime=lsf_walltime,
             local_directory="dask-worker-space",
             log_directory="dask-worker-logs",
-            env_extra=[f"conda activate {conda_env}"],
+            env_extra=[f"conda activate {lsf_env}"],
         )
         dask_cluster.scale(n=n_workers)
 
@@ -147,7 +177,7 @@ def label(
 
     futures = [
         dask_client.submit(label_molecule_batch, batched_molecules)
-        for batched_molecules in batch(enumerated_molecules, batch_size=500)
+        for batched_molecules in batch(enumerated_molecules, batch_size=batch_size)
     ]
 
     # Save out the molecules as they are ready.
